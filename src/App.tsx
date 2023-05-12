9d40050bf33699987fee77f162a96494ab69378c
Kishkin Vitalii <dimpla30@gmail.com>
2023-05-12 15:38:51 +0300

    popping the candies

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,17 @@ import { useAppDispatch, useAppSelector } from "./store/hooks";
 import { updateBoard } from "./store";
 import { createBoard } from "./utils/createBoard";
 import Board from "./components/Board";
+import {
+  checkForRowOfFour,
+  checkForRowOfThree,
+  isColumnOfFour,
+  isColumnOfThree,
+} from "./utils/moveCheckLogic";
+import {
+  formulaForColumnOfFour,
+  formulaForColumnOfThree,
+  generateInvalidMovies,
+} from "./utils/formulas";
 
 function App() {
   const dispatch = useAppDispatch();
@@ -15,6 +26,22 @@ function App() {
   useEffect(() => {
     dispatch(updateBoard(createBoard(boardSize)));
   }, [boardSize, dispatch]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const newBoard = [...board];
+      isColumnOfFour(newBoard, boardSize, formulaForColumnOfFour(boardSize));
+      checkForRowOfFour(
+        newBoard,
+        boardSize,
+        generateInvalidMovies(boardSize, true)
+      );
+      isColumnOfThree(newBoard, boardSize, formulaForColumnOfThree(boardSize));
+      checkForRowOfThree(newBoard, boardSize, generateInvalidMovies(boardSize));
+      dispatch(updateBoard(newBoard));
+    }, 150);
+    return () => clearInterval(timeout);
+  }, [board, boardSize, dispatch]);
 
   return (
     <div className="flex items-center justify-center h-screen">
---
+++ src/utils/formulas.ts
@@ -0,0 +1,18 @@
+export const formulaForColumnOfFour = (boardSize: number) =>
+  boardSize * boardSize - (boardSize + boardSize + boardSize) - 1;
+
+export const formulaForColumnOfThree = (boardSize: number) =>
+  boardSize * boardSize - (boardSize + boardSize) - 1;
+
+export const generateInvalidMovies = (
+  boardSize: number,
+  isFour: boolean = false
+) => {
+  const invalidMovies: Array<number> = [];
+  for (let i: number = boardSize; i <= boardSize * boardSize; i += boardSize) {
+    if (isFour) invalidMovies.push(i - 3);
+    invalidMovies.push(i - 2);
+    invalidMovies.push(i - 1);
+  }
+  return invalidMovies;
+};
